e63d8a5bc07994eb1091682e154981d2692204a3
skens06 <kenny.moutote@coding-academy.fr>
2019-09-23 14:26:44 +0200

    inutile

--- client/constants/Colors.js
+++ client/constants/Colors.js
@@ -1,14 +1,3 @@
-const tintColor = '#2f95dc';
-
-export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
-};
+export default{
+    primary: '#fc9208'
+}
