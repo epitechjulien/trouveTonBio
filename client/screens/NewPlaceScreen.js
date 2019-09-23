b767eaf6f631bf847c00bd8ad61657a0af709d44
Sangoku <julien.moukmir@coding-academy.fr>
2019-09-23 14:21:15 +0200

    second step screens app.js

--- client/screens/NewPlaceScreen.js
+++ client/screens/NewPlaceScreen.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import {ScrollView, Text, TextInput, Button, View, StyleSheet} from 'react-native';
+import Colors from '../constants/Colors';
+import { useDispatch } from 'react-redux';
+import * as placesActions from '../store/places-actions';
 
 
 const NewPlaceScreen = props => {
-    return( <View>
-        <Text>NewPlaceScreen</Text>
-    </View>
+    const [titleValue, setTitleValue] = useState('');
+
+const dispatch = useDispatch();
+
+const titleChangeHandler = text => {
+        //you could add validation
+        setTitleValue(text);
+    };
+
+
+
+const savePlaceHandler = () => {
+    dispatch(placeActions.addPlace(titleValue));
+    props.navigation.goBack();
+};
+
+    return( 
+    <ScrollView>
+        <View style={styles.form}>
+            <Text style={styles.label}>Title</Text>
+            <TextInput
+             style={styles.textInput}
+             onChangeText={titleChangeHandler}
+             value={titleValue}
+             />
+            <Button title="Save Place" 
+            color={Colors.primary} 
+            onPress={savePlaceHandler} />
+        </View>
+    </ScrollView>
     );
 };
 
@@ -15,6 +45,21 @@ headerTitle: 'Add Place'
 
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize:18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
 
 export default NewPlaceScreen;
