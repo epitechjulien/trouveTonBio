0271817a80794f85e5846aa5e0ecd317b85dc007
skens06 <kenny.moutote@coding-academy.fr>
2019-09-24 17:12:14 +0200

    Fetch and Select places from database

--- client/store/places-actions.js
+++ client/store/places-actions.js
@@ -2,7 +2,9 @@ import * as FileSystem from 'expo-file-system';
 
 
 export const ADD_PLACE = 'ADD_PLACE';
-import { insertPlace } from '../helpers/db';
+export const SET_PLACES = 'SET_PLACES';
+import { insertPlace , fetchPlaces } from '../helpers/db';
+import { async } from 'q';
 
 export const addPlace = (title, image) => {
   return async dispatch => {
@@ -30,5 +32,19 @@ export const addPlace = (title, image) => {
     }
 
 
+  };
+};
+
+export const loadPlaces = () => {
+  return async dispatch => {
+    try {
+      const dbResult = await fetchPlaces();
+      console.log(dbResult);
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+
+    }
+    catch (err){
+      throw err; 
+    }
   };
 };
