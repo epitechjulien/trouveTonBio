73fb389f4dd9802fa4dbee6bc53aca45902a042d
skens06 <kenny.moutote@coding-academy.fr>
2019-09-24 12:43:54 +0200

    access cam and image pick

--- client/components/ImagePicker.js
+++ client/components/ImagePicker.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
 import Colors from '../constants/Colors';
 
+//cam access and image pick
 const ImgPicker = props => {
+  const [pickedImage, setPickedImage] = useState();
   const verifyPermissions = async () => {
-    const result = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
     if (result.status !== 'granted') {
       Alert.alert(
         'Insufficient permissions!',
@@ -22,16 +24,25 @@ const ImgPicker = props => {
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermissions();
     if (!hasPermission) {
-        return;
+      return;
     }
-    ImagePicker.launchCameraAsync();
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
+    });
+    console.log(image);
+    setPickedImage(image.uri);
   };
 
   return (
     <View style={styles.imagePicker}>
       <View style={styles.imagePreview}>
+        {!pickedImage ? (
         <Text>No image picked yet.</Text>
-        <Image style={styles.image} />
+        ) : (
+        <Image style={styles.image} source={{uri: pickedImage}} />
+        )}
       </View>
       <Button
         title="Take Image"
