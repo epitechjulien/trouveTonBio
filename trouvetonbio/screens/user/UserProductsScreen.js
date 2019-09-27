cc23aa889f1f35745287135def017d67423d30ac
ben <benoit.rejou@coding-academy.fr>
2019-09-27 02:41:01 +0200

    Edited UserProd crud buttons

--- trouvetonbio/screens/user/UserProductsScreen.js
+++ trouvetonbio/screens/user/UserProductsScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { FlatList, Platform } from 'react-native';
+import { FlatList,Button, Platform } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
-
 import ProductItem from '../../components/shop/ProductItem';
+import Colors from '../../constants/Colors';
 
 const UserProductsScreen = props => {
     const userProducts = useSelector(state => state.products.userProducts);
@@ -19,9 +19,23 @@ const UserProductsScreen = props => {
                     image={itemData.item.imageUrl}
                     title={itemData.item.title}
                     price={itemData.item.price}
-                    onViewDetail={() => {}}
-                    onVAddToCart={() => {}}
-                    />
+                    onSelect={() => {}}
+                    >
+                    <Button
+                        color={Colors.primary}
+                        title="Edit"
+                        onPress={() => {
+                        
+                        }}
+              />
+              <Button
+                color={Colors.primary}
+                title="Delete"
+                onPress={() => {
+                 
+                }}
+              />
+                    </ProductItem>
               )}
             />
     );
