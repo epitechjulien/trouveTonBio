7c27872a92fce23abdf2a638537eb454feccbfcb
ben <benoit.rejou@coding-academy.fr>
2019-09-27 02:44:20 +0200

    Fixed UserProd crud buttons

--- trouvetonbio/screens/user/UserProductsScreen.js
+++ trouvetonbio/screens/user/UserProductsScreen.js
@@ -35,7 +35,7 @@ const UserProductsScreen = props => {
                  
                 }}
               />
-                    </ProductItem>
+                </ProductItem>
               )}
             />
     );
