b1c0e04bd4a110b730d7c482a0f0d9681019e850
ben <benoit.rejou@coding-academy.fr>
2019-09-27 02:00:08 +0200

    Added UserProd crud page mecanic

--- trouvetonbio/data/dummy-data.js
+++ trouvetonbio/data/dummy-data.js
@@ -28,7 +28,7 @@ export const PRODUCTS = [
     new Product(
       'p2',
       ['c1'],
-      ['owner2'],
+      'owner2',
       'Blue Carpet',
       'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       'Fits your red shirt perfectly. To stand on. Not to wear it.',
@@ -37,7 +37,7 @@ export const PRODUCTS = [
     new Product(
       'p3',
       ['c1'],
-      ['owner1'],
+      'owner1',
       'Coffee Mug',
       'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
       'Can also be used for tea!',
@@ -46,7 +46,7 @@ export const PRODUCTS = [
     new Product(
       'p4',
       ['c1'],
-      ['owner2'],
+      'owner2',
       'The Book - Limited Edition',
       'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
       "What the content is? Why would that matter? It's a limited edition!",
@@ -55,7 +55,7 @@ export const PRODUCTS = [
     new Product(
       'p5',
       ['c3'],
-      ['owner3'],
+      'owner3',
       'PowerBook',
       'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
       'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
@@ -64,7 +64,7 @@ export const PRODUCTS = [
     new Product(
       'p6',
       ['c4'],
-      ['owner4'],
+      'owner4',
       'Pen & Paper',
       'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
       "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
--- trouvetonbio/store/reducers/products.js
+++ trouvetonbio/store/reducers/products.js
@@ -1,7 +1,7 @@
 import PRODUCTS from '../../data/dummy-data';
 const initialState = {
     availableProducts: PRODUCTS,
-    userProducts: PRODUCTS.filter(prod => prod.ownerId=== 'u1')
+    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'owner1')
 };
 
 export default (state= initialState, action) => {
