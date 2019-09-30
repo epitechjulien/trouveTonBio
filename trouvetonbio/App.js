e094917b87528a85429f053b9ee8ed002f53e8af
Sangoku <julien.moukmir@coding-academy.fr>
2019-09-30 20:10:10 +0200

    fixed app.js

--- trouvetonbio/App.js
+++ trouvetonbio/App.js
@@ -1,51 +1,45 @@
-import React, {useState} from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { useScreens } from 'react-native-screens';
-
-// STORE import
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import cartReducer from './store/reducers/cart';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
 import productsReducer from './store/reducers/products';
-import ShopNavigator from './navigation/ShopNavigator';
+import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/orders';
+import ShopNavigator from './navigation/ShopNavigator';
 
-// Product import
-import ProductsNavigator from './navigation/ProductsNavigator';
-
-useScreens();
-
-// Font loading import
-const fetchFonts = () => {
-  Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
-// Reducer const combiner
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   orders: ordersReducer
 });
 
-// remove composeWithDev before deploying app
 const store = createStore(rootReducer);
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if(!fontLoaded) {
+  if (!fontLoaded) {
     return (
       <AppLoading
         startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
       />
     );
   }
-
-  return <ProductsNavigator />;
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
 }
