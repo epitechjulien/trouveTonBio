d6220ed1dec8fcd08c8ab68b6e6ec8d1a3c9b7b5
Siala <walid.siala@coding-academy.fr>
2019-10-03 13:04:50 +0200

    Fix conflict

--- trouvetonbio/models/product.js
+++ trouvetonbio/models/product.js
@@ -1,9 +1,5 @@
 class Product {
-<<<<<<< HEAD
-  constructor(id, categoryIds,subcategoriesIds, ownerId, title, image, description, price, promo) {
-=======
   constructor(id, categoryIds,subcategoriesIds, ownerId, title, image, description, price, promotion) {
->>>>>>> add promotion OK
     this.id = id;
     this.categoryIds=categoryIds;
     this.subcategoriesIds=subcategoriesIds;
@@ -12,11 +8,7 @@ class Product {
     this.title = title;
     this.description = description;
     this.price = price;
-<<<<<<< HEAD
-    this.promo = promo;
-=======
     this.promotion = promotion;
->>>>>>> add promotion OK
   }
 }
 
